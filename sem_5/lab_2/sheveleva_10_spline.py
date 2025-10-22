edbdf832452d092562cafd28564b2071deab1a6d
Daria Sheveleva <145582373+shevelevvvvvvva@users.noreply.github.com>
2025-10-22 18:09:46 +0300

    Update sheveleva_10_spline.py

--- sem_5/lab_2/sheveleva_10_spline.py
+++ sem_5/lab_2/sheveleva_10_spline.py
@@ -1,7 +1,10 @@
 import numpy as np
 
-x = np.array([0.87267, 1.22173, 1.57080, 1.91986, 2.26893, 2.61799, 2.96706])
-y = np.array([0.00123, 0.01343, 0.08411, 0.37341, 1.31146, 3.88447, 10.10742])
+#x = np.array([0.87267, 1.22173, 1.57080, 1.91986, 2.26893, 2.61799, 2.96706])
+#y = np.array([0.00123, 0.01343, 0.08411, 0.37341, 1.31146, 3.88447, 10.10742])
+
+x = np.array([0, 1, 2, 3, 4, 5, 6])
+y = np.array([5, 3, 11, 17, -15, -145, -457])
 
 n = len(x)
 
@@ -14,6 +17,7 @@ for j in range(1, n):
 
 b = dd[0, :n].copy()
 
+# Полином Ньютона
 poly = np.poly1d([0.0])
 for k in range(n):
     term = np.poly1d([1.0])
@@ -24,50 +28,69 @@ for k in range(n):
 
 standard_coeffs = poly.coefficients
 
-print("\nКоэффициенты стандартного полинома:")
+print("Коэффициенты стандартного полинома:")
 for i, c in enumerate(standard_coeffs):
     power = len(standard_coeffs) - i - 1
     print(f"a_{power} = {c:.12g}")
 
-x0 = float(input("\nВведите значение x0 для полинома: "))
-y0 = poly(x0)
-print(f"P({x0}) = {y0}")
-
-poly_der = poly.deriv() # Производная интерполяционного полинома
+# Производная полинома
+poly_der = poly.deriv()
 Pp = poly_der(x)  # значения производной в узлах
 
-spline_coeffs = []
+print("\nПроизводные в узлах:")
+for i in range(n):
+    print(f"P'({x[i]:.5f}) = {Pp[i]:.12g}")
+
+
+spline_coeffs_cpp = []
 
 for i in range(n - 1):
     xi, xi1 = x[i], x[i + 1]
     fi, fi1 = y[i], y[i + 1]
-    dpi, dpi1 = Pp[i], Pp[i + 1]
+    mi, mi1 = Pp[i], Pp[i + 1]  # производные в узлах
     h = xi1 - xi
-    denom = h ** 3
+    h3 = h * h * h
 
-    a3 = (dpi1 * h - 2 * (fi1 - fi) + dpi * h) / denom
-    a2 = (-dpi1 * h * (xi1 + 2 * xi) + 3 * (fi1 - fi) * (xi1 + xi) - dpi * h * (xi + 2 * xi1)) / denom
-    a1 = (dpi1 * xi1 * (xi1 + 2 * xi) - 6 * (fi1 - fi) * xi1 + dpi * xi * (xi1 + 2 * xi)) / denom
-    a0 = (-dpi1 * xi1 ** 2 * h + fi1 * xi1 ** 2 * (3 * xi - xi1) + fi * xi ** 2 * (xi - 3 * xi1)
-          - dpi * xi ** 2 * h) / denom
+    num_a3 = mi1 * h + mi * h - 2 * (fi1 - fi)
+    a3 = num_a3 / h3
 
-    spline_coeffs.append((a3, a2, a1, a0))
+    num_a2 = -mi1 * h * (xi1 + 2 * xi) + 3 * (fi1 - fi) * (xi1 + xi) - mi * h * (xi + 2 * xi1)
+    a2 = num_a2 / h3
 
-print("\nКоэффициенты кубического сплайна по интервалам:")
-for i, (a3, a2, a1, a0) in enumerate(spline_coeffs):
-    print(f"[{x[i]:.5f}, {x[i+1]:.5f}]  →  a3={a3:.6f}, a2={a2:.6f}, a1={a1:.6f}, a0={a0:.6f}")
+    num_a1 = mi1 * xi * (2 * xi1 + xi) * h - 6 * (fi1 - fi) * xi * xi1 + mi * xi1 * (xi1 + 2 * xi) * h
+    a1 = num_a1 / h3
+
+    num_a0 = -mi1 * pow(xi, 2) * xi1 * h + fi1 * pow(xi, 2) * (3 * xi1 - xi) + fi * pow(xi1, 2) * (
+                xi1 - 3 * xi) - mi * xi * pow(xi1, 2) * h
+    a0 = num_a0 / h3
+
+    spline_coeffs_cpp.append((a0, a1, a2, a3, xi))
+
+print("\nКоэффициенты кубического сплайна:")
+for i, (a0, a1, a2, a3, start) in enumerate(spline_coeffs_cpp):
+    print(f"[{x[i]:.5f}, {x[i + 1]:.5f}]")
+    print(f"  a0 = {a0:.12g}")
+    print(f"  a1 = {a1:.12g}")
+    print(f"  a2 = {a2:.12g}")
+    print(f"  a3 = {a3:.12g}")
 
 
-def spline(x0):
-    for i in range(n - 1):
-        if x[i] <= x0 <= x[i + 1]:
-            a3, a2, a1, a0 = spline_coeffs[i]
-            dx = x0 - x[i]
-            return a3 * dx**3 + a2 * dx**2 + a1 * dx + a0
+def spline_cpp(x0, spline_coeffs, nodes):
+    for i in range(len(nodes) - 1):
+        if nodes[i] <= x0 <= nodes[i + 1]:
+            a0, a1, a2, a3, start = spline_coeffs[i]
+            return a0 + a1 * x0 + a2 * x0 ** 2 + a3 * x0 ** 3
     return None
 
 
-print("\nТаблица приближённых значений функции по кубическому сплайну:")
-X_values = np.linspace(x[0], x[-1], 10)  # 10 точек внутри интервала интерполяции
-for X in X_values:
-    print(f"S({X:.5f}) = {spline(X):.8f}")
+print("\nЗначения сплайна в точках")
+test_points = [1.0, 1.65, 2.0, 2.5]
+for x0 in test_points:
+    result = spline_cpp(x0, spline_coeffs_cpp, x)
+    print(f"S({x0}) = {result:.12g}")
+
+print("\nЗначения интерполяционного многочлена в точках:")
+test_points = [1.0, 1.65, 2.0, 2.5]
+for x0 in test_points:
+    result = poly(x0)
+    print(f"P({x0}) = {result:.12g}")
